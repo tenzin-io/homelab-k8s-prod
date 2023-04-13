543f5f516a99c0a02fe63861d879599e88f0c595
Tenzin Lhakhang <2402007+tlhakhan@users.noreply.github.com>
2023-04-13 18:49:24 -0400

    Update module version and module name change

--- main.tf
+++ main.tf
@@ -7,33 +7,16 @@ terraform {
   }
 }
 
-provider "aws" {
-  region = "us-east-1"
-}
-
-provider "helm" {
-  kubernetes {
-    config_path    = "kubernetes-admin.conf"
-    config_context = "kubernetes-admin@kubernetes"
-  }
-}
-
-provider "kubernetes" {
-  config_path    = "kubernetes-admin.conf"
-  config_context = "kubernetes-admin@kubernetes"
-}
-
 module "cert_manager" {
-  source               = "git::https://github.com/tenzin-io/terraform-tenzin-cert-manager.git?ref=v0.0.1"
-  cloudflare_api_email = "tenzin@tenzin.io"
-  cloudflare_api_token = data.aws_ssm_parameter.cloudflare_api_token.value
+  source               = "git::https://github.com/tenzin-io/terraform-tenzin-cert-manager.git?ref=v0.0.2"
+  cert_registration_email = "tenzin@tenzin.io"
+  cloudflare_api_token = chomp(data.aws_ssm_parameter.cloudflare_api_token.value)
 }
 
-module "github_actions_runner" {
+module "github_actions" {
   source                     = "git::https://github.com/tenzin-io/terraform-tenzin-github-actions-runner-controller.git?ref=v0.0.1"
   github_org_name            = "tenzin-io"
   github_app_id              = data.aws_ssm_parameter.github_app_id.value
   github_app_installation_id = data.aws_ssm_parameter.github_app_installation_id.value
   github_app_private_key     = data.aws_ssm_parameter.github_app_private_key.value
-  depends_on                 = [module.cert_manager]
 }
---
+++ providers.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+provider "helm" {
+  kubernetes {
+    config_path    = "kubernetes-admin.conf"
+  }
+}
+
+provider "kubernetes" {
+  config_path    = "kubernetes-admin.conf"
+}
