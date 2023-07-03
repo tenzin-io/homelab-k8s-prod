3b1aa8117fabda9e42084400cf6ec4b244e2a7aa
Tenzin Lhakhang <2402007+tlhakhan@users.noreply.github.com>
2023-07-03 14:32:43 -0400

    Move github_actions to release version

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ module "cert_manager" {
 }
 
 module "github_actions" {
-  source                     = "git::https://github.com/tenzin-io/terraform-tenzin-github-actions-runner-controller.git?ref=main"
+  source                     = "git::https://github.com/tenzin-io/terraform-tenzin-github-actions-runner-controller.git?ref=v0.2.0"
   github_org_name            = "tenzin-io"
   github_app_id              = data.vault_generic_secret.github_app.data.app_id
   github_app_installation_id = data.vault_generic_secret.github_app.data.installation_id
